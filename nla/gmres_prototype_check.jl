2c95300af2f7269061ecd8b006fd2a328b2fdbca
sandreza <andrenogueirasouza@gmail.com>
2020-04-19 19:22:44 -0500

    new file that tests it

---
+++ nla/gmres_prototype_check.jl
@@ -0,0 +1,15 @@
+include("gmres_prototype.jl")
+using LinearAlgebra, Plots, Random
+n = 100 # size of vector space
+Random.seed!(1235)
+b = randn(n) # rhs
+x = randn(n) # initial guess
+A = randn((n,n)) ./ sqrt(n) .* 1.0 + I
+gmres = PrototypeRes(b)
+linear_operator! = closure_linear_operator!(A)
+
+r = solve!(x, b, linear_operator!, gmres; iterations = length(b), residual = true)
+
+###
+# Now just plot the convergence rate
+scatter(log.(r)/log(10), xlabel = "iteration", ylabel = "log10 residual", title = "gmres convergence", legend = false)
